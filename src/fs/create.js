f576e12228eb0fe8931134d78422ff882cdde065
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2022-05-31 00:58:52 +0300

    feat: add solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,23 @@
+import { writeFile, stat } from "fs";
+import { Buffer } from "buffer";
+import * as path from 'path'
+
+
 export const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const msg = "I am fresh and young\n";
+  const fileName = "fresh.txt";
+  const p = path.join("files",fileName);
+  const data = new Uint8Array(Buffer.from(msg));
+  stat(p, (err) => {
+    if (err) {
+      writeFile(p, data, (err) => {
+        if (err) throw new Error("FS operation failed");
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
+
+create();
