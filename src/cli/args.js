97933eb254a3fd7646ecc4c6569fb49d841cb2d2
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2022-05-31 22:48:02 +0300

    feat: add solution for cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
 export const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const res = [];
+  const n = process.argv.length;
+  for (let i = 2; i < n; i++) {
+    const propName = process.argv[i].slice(2);
+    const propValue = process.argv[i + 1];
+    i++;
+    res.push(`${propName} is ${propValue}`);
+  }
+  console.log(res.join(", "));
 };
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,12 @@
 export const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const res = [];
+  for (let key in process.env) {
+    const prefix = key.slice(0, 4);
+    if (prefix === "RSS_") {
+      res.push(`${key}=${process.env[key]}`);
+    }
+  }
+  console.log(res.join("; "));
 };
+parseEnv();
