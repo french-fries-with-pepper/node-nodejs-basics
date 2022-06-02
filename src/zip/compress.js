317b0d1d29d030608f2054e7cf423a208864a981
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2022-06-03 02:24:10 +0300

    feat: add solutions for zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,23 @@
+import { createReadStream, createWriteStream } from "fs";
+import * as zlib from "zlib";
+import * as path from "path";
+
 export const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const inputPath = path.join(path.resolve(), "files", "fileToCompress.txt");
+  const outPath = path.join(path.resolve(), "files", "archive.gz");
+  const readSteam = createReadStream(inputPath);
+  const writeStream = createWriteStream(outPath);
+  const gzipStream = zlib.createGzip();
+
+  readSteam.on("error", () => {
+    throw new Error("FS operation failed");
+  });
+  writeStream.on("error", () => {
+    throw new Error("FS operation failed");
+  });
+
+  readSteam.pipe(gzipStream).pipe(writeStream);
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,23 @@
+import { createReadStream, createWriteStream } from "fs";
+import * as zlib from "zlib";
+import * as path from "path";
+
 export const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const inputPath = path.join(path.resolve(), "files", "archive.gz");
+  const outPath = path.join(path.resolve(), "files", "fileToCompress.txt");
+  const readSteam = createReadStream(inputPath);
+  const writeStream = createWriteStream(outPath);
+  const gzipUnpackStream = zlib.createUnzip();
+
+  readSteam.on("error", () => {
+    throw new Error("FS operation failed");
+  });
+  writeStream.on("error", () => {
+    throw new Error("FS operation failed");
+  });
+
+  readSteam.pipe(gzipUnpackStream).pipe(writeStream);
 };
+
+decompress();
