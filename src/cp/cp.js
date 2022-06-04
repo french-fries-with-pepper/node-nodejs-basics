45e266e0c3e1cbcb4fa6b082219fcb44763be7ae
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2022-06-04 03:20:35 +0300

    feat: add solution for child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,20 @@
+import * as path from "path";
+import { fork } from "child_process";
+
 export const spawnChildProcess = async (args) => {
     // Write your code here
+    const modulePath = path.join(path.resolve(), "files","script");
+    const child = fork(modulePath,[...args],{silent:true});
+    process.stdin.on("data", (data)=>{
+        child.stdin.write(data);
+    })
+    child.stdout.on("data", (data) =>{
+        process.stdout.write(data);
+    })
+    child.on("exit", () =>{
+        console.log("child process was ended, so nothing to do...");
+        process.exit(0);
+    })
 };
+
+spawnChildProcess(["A","B","C"]);
